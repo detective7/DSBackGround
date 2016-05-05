de32de82c3c7f5e81d1a2f659b5536c5eea6152d
detective7 <detective.ive@163.com>
2016-05-05 23:39:31 +0800

    资料分享系统客户端

--- tijiaotongji.php
+++ tijiaotongji.php
@@ -1,5 +1,6 @@
 <?php
 include ("conn.php"); //连接数据库
 $hw_id = $_POST['hw_id'];
-
+$sql_t= "select pick_up.stu_id from `work`,pick_up,user_from_osystem "
+		."where `work`.id_work=27 and `work`.id_work=pick_up.hw_id and pick_up.stu_id = user_from_osystem.user_number and `work`.class=user_from_osystem.user_class"
 ?>
